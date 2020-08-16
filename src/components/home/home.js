26639ab73c7aac16397e4630e464159042e59ce7
broberts2 <broc@compliancemonitoringsystems.com>
2020-08-15 19:47:15 -0600

    changes

---
+++ src/components/home/home.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Particles } from "arclight-react";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div className={"home"}>
+      <Particles style={"a"} />
+      <h1>Arclight</h1>
+    </div>
+  );
+}
