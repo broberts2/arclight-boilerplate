735116eaba931f04cbf703c7e49e2cd1eb8e1f8d
broberts2 <broc.roberts@outlook.com>
2021-01-04 01:22:04 -0700

    changes

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -3,6 +3,6 @@ const { spawn } = require("child_process");
 module.exports = (app, pretext) => {
   app.post(`/gitUpdate`, async (req, res) => {
     const ls = spawn("ls");
-    res.json("success"); //
+    res.json("success");
   });
 };
