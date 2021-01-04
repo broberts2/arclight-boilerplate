8f304fa975a9631e8f8ff82e39268f7db25cec80
broberts2 <broc.roberts@outlook.com>
2021-01-04 00:48:24 -0700

    changes

--- merge.sh
+++ merge.sh
@@ -5,4 +5,7 @@ rm -rf /var/www/html/arclight.cc/build
 mv build /var/www/html/arclight.cc
 cd /arclight-react
 git pull origin master
+npm run build
+rm -rf /var/www/html/arclight-react/build
+mv build /var/www/html/arclight-react
 sudo systemctl restart httpd.service
