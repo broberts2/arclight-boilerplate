42d6ffbc897afc9b107de954f4fc95105134f619
broberts2 <broc.roberts@outlook.com>
2021-01-04 00:54:05 -0700

    changes

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -3,7 +3,6 @@ const shell = require("shelljs");
 module.exports = (app, pretext) => {
   app.post(`/gitUpdate`, async (req, res) => {
     shell.exec("../merge.sh");
-    //test
     res.json("success");
   });
 };
