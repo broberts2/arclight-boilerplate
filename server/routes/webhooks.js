86f791f18cf8d32be24936892f8f38a91c858b1b
broberts2 <broc.roberts@outlook.com>
2021-01-04 01:19:42 -0700

    changes

--- server/routes/webhooks.js
+++ server/routes/webhooks.js
@@ -3,6 +3,6 @@ const { exec } = require("child_process");
 module.exports = (app, pretext) => {
   app.post(`/gitUpdate`, async (req, res) => {
     exec("echo running merge");
-    res.json("success");
+    res.json("success"); //
   });
 };
